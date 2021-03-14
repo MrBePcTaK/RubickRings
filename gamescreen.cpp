3b4ad902b24cccd071e42e2b60b6a1682f23a694
MrBePcTaK <57495394+MrBePcTaK@users.noreply.github.com>
2021-03-14 21:28:32 +0700

    Add project

---
+++ gamescreen.cpp
@@ -0,0 +1,15 @@
+#include "gamescreen.h"
+#include "ui_gamescreen.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
